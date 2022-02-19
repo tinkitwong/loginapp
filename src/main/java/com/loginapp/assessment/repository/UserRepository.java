b8b80bb8b66644fb9da90fc68311b3616cf07dc4
tinkitwong <tinkitwong@gmail.com>
2022-02-20 00:07:31 +0800

    Added Spring Security to Backend

--- src/main/java/com/loginapp/assessment/repository/UserRepository.java
+++ src/main/java/com/loginapp/assessment/repository/UserRepository.java
@@ -7,5 +7,5 @@ import com.loginapp.assessment.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository <User, Long> {
-
+	User findByEmail(String email);
 }
--- src/main/java/com/loginapp/assessment/service/UserService.java
+++ src/main/java/com/loginapp/assessment/service/UserService.java
@@ -1,9 +1,11 @@
 package com.loginapp.assessment.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.loginapp.assessment.dto.UserRegistrationDto;
 import com.loginapp.assessment.model.User;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	
 	User save(UserRegistrationDto registrationDto);
 
