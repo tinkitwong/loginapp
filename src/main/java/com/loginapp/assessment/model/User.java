c5e77ca7575bdd8d3c2a0b06782d3e63650ac520
tinkitwong <tinkitwong@gmail.com>
2022-02-19 17:18:22 +0800

    + Added M2M User:Roles

--- src/main/java/com/loginapp/assessment/model/User.java
+++ src/main/java/com/loginapp/assessment/model/User.java
@@ -2,13 +2,18 @@ package com.loginapp.assessment.model;
 
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 @Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
@@ -25,6 +30,19 @@ public class User {
 	private String lastName;
 	private String email;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "user_roles",
+			joinColumns = @JoinColumn(
+					name = "user_id",
+					referencedColumnName = "id"
+					),
+			inverseJoinColumns = @JoinColumn(
+					name = "role_id",
+					referencedColumnName = "id"
+					)
+			)
 	private Collection <Role> roles;
 	
 	
@@ -87,7 +105,7 @@ public class User {
 		this.password = password;
 	}
 
-
+	
 	public Collection<Role> getRoles() {
 		return roles;
 	}
